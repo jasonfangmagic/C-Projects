76ec3ff6178ae8687c8a839a49564a0f71450b7f
Jason Fang <jasonfangmagic@gmail.com>
2021-08-13 12:58:34 -0400

    C++

---
+++ cmake-build-debug/CMakeFiles/C__Projects.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C__Projects.dir/main.cpp.obj"
+  "C__Projects.exe"
+  "C__Projects.exe.manifest"
+  "C__Projects.pdb"
+  "libC__Projects.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C__Projects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
