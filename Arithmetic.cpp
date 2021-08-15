ce52d18c49072d6bf5c89a9fdfb9df2e79a92195
jasonfangmagic <jasonfangmagic@gmail.com>
2021-08-15 19:43:53 -0400

    C++

---
+++ Arithmetic.cpp
@@ -0,0 +1,21 @@
+#include "iostream"
+#include "string"
+using namespace std;
+
+int main(){
+    int x = 8;
+    int y = 9;
+    int sum = x + y;
+    cout << sum;
+
+
+}
+
+/*
+ +
+ -
+ /
+ *
+ ++
+ --
+ */
