ea01613e1a65230d507944945d10c996309cec74
jasonfangmagic <jasonfangmagic@gmail.com>
2021-08-15 16:22:51 -0400

    C++

--- DataTypes.cpp
+++ DataTypes.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
-    int
-    float
-    bool
-    string
-    char 
+    int x = 2;
+    cout << x;
+
 }
+
+/*
+    data types
+    int 7,89,23
+    float 8.0, 3.432
+    bool true,false
+    string "asd", "fuh"
+    char 'z' 's'
+*/
