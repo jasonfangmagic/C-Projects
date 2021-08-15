6bbdfee0d90c2b9f5501ebb83963f547db095275
jasonfangmagic <jasonfangmagic@gmail.com>
2021-08-15 18:19:55 -0400

    C++

---
+++ calculator.cpp
@@ -0,0 +1,17 @@
+#include "iostream"
+#include "string"
+using namespace std;
+
+int main() {
+    int num1, num2;
+    cout <<"Enter a Number: ";
+    cin >> num1;
+    cin.clear();
+    cin.ignore(100000,'\n')
+    cout <<"Enter a 2nd Number: ";
+    cin >> num2;
+    int sum = num1 +num2;
+    cout << "The sum is: " <<sum;
+
+}
+
