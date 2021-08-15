effe9a7efa85f2aa7e0b642040cf9666c11cc108
jasonfangmagic <jasonfangmagic@gmail.com>
2021-08-14 20:46:15 -0400

    C++

---
+++ cmake-build-debug/CMakeFiles/C__Projects.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for C__Projects.
+# This may be replaced when dependencies are built.
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/C++Projects
+# Install script for directory: C:/Users/jason/C-Projects
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/C++Projects/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/jason/C-Projects/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
