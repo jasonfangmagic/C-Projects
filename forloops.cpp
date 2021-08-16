b470e9acada932ee8d1c974888df7542de65fbe1
Jason Fang <jasonfangmagic@gmail.com>
2021-08-16 16:56:13 -0400

    C++

--- forloops.cpp
+++ forloops.cpp
@@ -3,9 +3,23 @@
 using namespace std;
 
 int main () {
-    for (int i = 0; i < 10; i++) {
-        cout << i << endl;
+    int x [] = {5, 6, 7, 8, 9, 10, 11, 12, 20, 30, 40, 50};
 
+    for (int i = 0; i < sizeof (x)/sizeof (x[0]); i++) {
+
+            cout << x[i] << endl;
     }
 
 }
+
+/*
+int main () {
+
+    for (int i = 10; i > 0; i-=4) {
+        if (i % 2 == 0){
+            cout << i << endl;
+        }
+
+    }
+
+}*/
