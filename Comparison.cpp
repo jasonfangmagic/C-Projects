61939baa573865aa498df5cbb574287e9bb01346
jasonfangmagic <jasonfangmagic@gmail.com>
2021-08-15 20:19:50 -0400

    C++

--- Comparison.cpp
+++ Comparison.cpp
@@ -3,8 +3,12 @@
 using namespace std;
 
 int main(){
-    bool test = 1 < 2;
-    cout<< test;
+    int num1, num2;
+    cout << "Type a number: ";
+    cin >> num1;
+    cout << "Type a 2nd number: ";
+    cin >> num2;
+    cout << "This two numbers are the same: " << (num1 == num2);
 }
 
 /*
