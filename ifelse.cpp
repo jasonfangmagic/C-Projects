227181240f5622f65d3bb61740908e2c29bb7990
Jason Fang <jasonfangmagic@gmail.com>
2021-08-16 12:14:53 -0400

    C++

---
+++ ifelse.cpp
@@ -0,0 +1,16 @@
+#include "iostream"
+#include "string"
+using namespace std;
+
+int main (){
+    int x = 2 ;
+    if (x < 7 ) {
+        cout << "x is less than 7! " << endl;
+        if (x < 3){
+            cout << "x is less than 3";
+        }
+
+    }
+
+
+}
