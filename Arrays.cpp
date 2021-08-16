0207070943e0e71f3b68bb2ecb3e1879dabbca27
Jason Fang <jasonfangmagic@gmail.com>
2021-08-16 13:25:13 -0400

    C++

--- Arrays.cpp
+++ Arrays.cpp
@@ -2,8 +2,29 @@
 #include "string"
 using namespace std;
 
+
+int main(){
+    int arr[] = {11, 2, 3, 4, 5};
+    string arr2[] {"Jason", "Fang"};
+    float arr3[] {1.1, 2.3, 3.4};
+    cout<< arr[0] << endl;
+
+    //size of the array//
+    cout << sizeof(arr)/sizeof(arr[0]);
+}
+
+
+
+
+
+
+
+
+/*
+
 int main(){
     int arr[5] = {11, 2, 3, 4, 5};
     cout<< arr[0];
 }
+*/
 
