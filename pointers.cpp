dbacad92cfcca7143c4d5d5b045139a9651c0d79
Jason Fang <jasonfangmagic@gmail.com>
2021-08-17 12:18:31 -0400

    C++

--- pointers.cpp
+++ pointers.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 
 int main() {
-    int x = 2;
-    int *y = &x;
-    cout << &x << endl;
-    cout << y << endl;
+    int x[] = {1, 2, 3};
+    int *head = x;
+    cout << head << endl;
+
+    for ( int i =0; i < 3; ++i){
+        head = x + i;
+        cout << head <<endl;
+    }
 
 }
