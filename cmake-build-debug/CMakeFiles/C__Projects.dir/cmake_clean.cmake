8d4fa9e73472ec206ca4ad4e93d94c5a5896abbe
jasonfangmagic <jasonfangmagic@gmail.com>
2021-08-17 21:04:12 -0400

    C++

--- cmake-build-debug/CMakeFiles/C__Projects.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/C__Projects.dir/cmake_clean.cmake
@@ -1,5 +1,5 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/C__Projects.dir/sets.cpp.obj"
+  "CMakeFiles/C__Projects.dir/functions.cpp.obj"
   "C__Projects.exe"
   "C__Projects.exe.manifest"
   "C__Projects.pdb"
---
+++ functions.cpp
@@ -0,0 +1,12 @@
+#include "iostream"
+using namespace std;
+
+int add(int x ,int y) {
+    return x*y;
+}
+
+int main() {
+    int result = add(2,3);
+    cout << result <<endl;
+}
+
