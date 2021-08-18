f82e5cb9311d268b42b79caf8ffc1370a0c0dddb
jasonfangmagic <jasonfangmagic@gmail.com>
2021-08-17 20:44:37 -0400

    C++

--- vectors.cpp
+++ vectors.cpp
@@ -4,7 +4,12 @@ using namespace std;
 
 int main() {
     vector<int> v1 = {1, 2, 3};
-    v1.push_back(9);
-    cout << v1.size() << endl;
+    for (int i = 0; i < v1.size(); ++i) {
+        cout << v1[i] << endl;
+    }
+    v1.insert(v1.begin(), 10);
+    for (auto itr = v1.begin(); itr != v1.end(); ++itr){
+        cout << *itr <<endl;
+    }
 }
 
